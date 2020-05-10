d083ef38ec5672be9e3145c4bacf86405e67e816
Maxime Monrat <max.moon.rat@gmail.com>
2020-05-10 19:35:57 -0400

    Class enevelope added. Need to be tested.

---
+++ envelope.cpp
@@ -0,0 +1,197 @@
+#include "envelope.h"
+
+#pragma region Constructors
+
+envelope::envelope(float* time, float attack, float decay, float sustain, float release) : t(time)
+{ 
+	 _attack = attack > 0 ? attack : 0;	
+	_decay = decay > 0 ? decay : 0;
+	if (sustain > 1) _sustain = 1;
+	else if (sustain < 0) _sustain = 0;
+	else _sustain = sustain;
+	_release = release > 0 ? release : 0;
+
+	level = 0;
+	gate = 0;
+	gateUp = false;
+	gateDown = false;
+	gateUpTime = 0;
+	gateDownTime = 0;
+	currentStage = None;
+}
+
+#pragma endregion Constructors
+
+#pragma region Getters
+
+float envelope::GetAttack() const
+{
+	return _attack;
+}
+float envelope::GetDecay() const
+{
+	return _decay;
+}
+float envelope::GetSustain() const
+{
+	return _sustain;
+}
+float envelope::GetRelease() const
+{
+	return _release;
+}
+float envelope::GetLevel()
+{
+	Update();
+	return level;
+}
+envelope::stage envelope::GetCurrentStage() const
+{
+	return currentStage;
+}
+
+#pragma endregion Getters
+
+#pragma region Setters
+
+void envelope::SetAttack(float a)
+{
+	_attack = a;
+}
+void envelope::SetDecay(float d)
+{
+	_decay = d;
+}
+void envelope::SetSustain(float s)
+{
+	_sustain = s;
+}
+void envelope::SetRelease(float r)
+{
+	_release = r;
+}
+void envelope::SetGate(float g)
+{
+	if (g > 0 && !gateUp)
+	{
+		gateUp = true;
+		gateUpTime = *t;
+		gateUp = false;
+	}
+	else if (g <= 0 && !gateDown)
+	{
+		gateDown = true;
+		gateDownTime = *t;
+		gateDown = false;
+	}
+	gate = g;
+	Update();
+}
+
+#pragma endregion Setters
+
+#pragma region Public Methods
+
+envelope envelope::Add(envelope const& e) const
+{
+	return envelope(t, _attack + e._attack, _decay + e._decay, _sustain + e._sustain, _release + e._release);
+}
+envelope envelope::Subtract(envelope const& e) const
+{
+	return envelope(t, _attack - e._attack, _decay - e._decay, _sustain - e._sustain, _release - e._release);
+}
+envelope envelope::Multiply(envelope const& e) const
+{
+	return envelope(t, _attack * e._attack, _decay * e._decay, _sustain * e._sustain, _release * e._release);
+}
+
+#pragma endregion Public Methods
+
+#pragma region Private Methods
+
+/// <summary>
+/// <para>Updates the envelope level value</para>
+/// </summary>
+void envelope::Update()
+{
+	UpdateStage();
+	switch (currentStage)
+	{
+	case None:
+		break;
+
+	case Sustain:
+		level = gate * _sustain;
+		break;
+
+	case Release:
+		level = _sustain - (_sustain / _release) * (*t - gateDownTime);
+		break;
+
+	case Attack:
+		level = gate * (1 / _attack) * (*t - gateUpTime);
+		break;
+
+	case Decay:
+		level = gate * (1 - (1 - _sustain) / _decay) * (*t - (gateUpTime + _attack));
+		break;
+	}
+}
+
+void envelope::UpdateStage()
+{
+	if (gate > 0)
+	{
+		if (*t - gateUpTime < _attack) currentStage = Attack;
+		else if (*t - gateUpTime < _attack + _decay) currentStage = Decay;
+		else currentStage = Sustain;
+	}
+	else if (gate <= 0 && *t - gateDownTime < _release) currentStage = Release;
+	else currentStage = None;
+}
+
+#pragma endregion Private Methods
+
+#pragma region Operators
+
+envelope& envelope::operator+=(envelope const& e)
+{
+	_attack += e._attack;
+	_decay += e._decay;
+	_sustain += e._sustain;
+	_release += e._release;
+
+	return *this;
+}
+envelope& envelope::operator-=(envelope const& e)
+{
+	_attack -= e._attack;
+	_decay -= e._decay;
+	_sustain -= e._sustain;
+	_release -= e._release;
+
+	return *this;
+}
+envelope& envelope::operator*=(envelope const& e)
+{
+	_attack *= e._attack;
+	_decay *= e._decay;
+	_sustain *= e._sustain;
+	_release *= e._release;
+	
+	return *this;
+}
+envelope operator+(envelope const& a, envelope const& b)
+{
+	return a.Add(b);
+}
+envelope operator-(envelope const& a, envelope const& b)
+{
+	return a.Subtract(b);
+}
+envelope operator*(envelope const& a, envelope const& b)
+{
+	return a.Multiply(b);
+}
+
+#pragma endregion Operators
---
+++ envelope.h
@@ -0,0 +1,59 @@
+#ifndef DEF_ENVELOPE
+#define DEF_ENVELOPE
+
+
+class envelope
+{
+	public:
+		enum stage { None, Attack, Decay, Sustain, Release };
+
+		envelope(float* time, float attack = 0.2, float decay = 0.2, float sustain = 0.5, float release = 0.4);
+
+		float GetAttack() const;
+		void SetAttack(float a);
+
+		float GetDecay() const;
+		void SetDecay(float d);
+
+		float GetSustain() const;
+		void SetSustain(float s);
+
+		float GetRelease() const;
+		void SetRelease(float r);
+
+		float GetLevel();
+		stage GetCurrentStage() const;
+
+		void SetGate(float g);
+
+		envelope Add(envelope const& e) const;
+		envelope Subtract(envelope const& e) const;
+		envelope Multiply(envelope const& e) const;
+
+		envelope& operator+=(envelope const& e);
+		envelope& operator-=(envelope const& e);
+		envelope& operator*=(envelope const& e);		
+
+	private:		
+		stage currentStage;
+		float gate;
+		bool gateUp;
+		bool gateDown;
+		float gateUpTime; // time key of the last time trigger was set to true
+		float gateDownTime; // time key of the last time gate went back to 0
+		float _attack; // time taken to get from 0 to 1
+		float _decay; // time take to get from 1 to the sustained level
+		float _sustain; // sustain level
+		float _release; // time to get from the sustained level back to 0		
+		float level;		
+		float* t; // time in s
+		
+		void Update();
+		void UpdateStage();
+};
+
+envelope operator+(envelope const& a, envelope const& b);
+envelope operator-(envelope const& a, envelope const& b);
+envelope operator*(envelope const& a, envelope const& b);
+
+#endif
